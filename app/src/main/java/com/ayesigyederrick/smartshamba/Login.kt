951125449b1353f7bd498eafb9e4dd1a958296bb
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-06-14 12:43:49 +0300

    Finalized on Register and Login

--- app/src/main/java/com/ayesigyederrick/smartshamba/Login.kt
+++ app/src/main/java/com/ayesigyederrick/smartshamba/Login.kt
@@ -3,7 +3,12 @@ package com.ayesigyederrick.smartshamba
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +19,27 @@ class Login : AppCompatActivity() {
         val gotoRegisterPage = findViewById<TextView>(R.id.text_gotoRegister)
         gotoRegisterPage.setOnClickListener{
             startActivity(Intent(this, Register::class.java))
+        }
+    }
+    fun GotoMain2(view: View) {
+        val email = findViewById<TextInputEditText>(R.id.email_login).text.toString()
+        val password = findViewById<TextInputEditText>(R.id.password_login).text.toString()
+        if(email =="" || password == ""){
+            Toast.makeText(applicationContext, "Fields cant be left Empty, Go to Register to open an Account", Toast.LENGTH_LONG).show()
+        }else {
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val i = Intent(this, MainActivity::class.java)
+                        startActivity(i)
+                        Toast.makeText(applicationContext, "Login successful", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext,
+                        exception.localizedMessage,
+                        Toast.LENGTH_LONG).show()
+                }
         }
     }
 }
--- app/src/main/java/com/ayesigyederrick/smartshamba/Register.kt
+++ app/src/main/java/com/ayesigyederrick/smartshamba/Register.kt
@@ -3,7 +3,12 @@ package com.ayesigyederrick.smartshamba
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +16,35 @@ class Register : AppCompatActivity() {
         setContentView(R.layout.activity_register)
         title = "SignUP"
 
+
         val gotoLoginPage = findViewById<TextView>(R.id.text_gotoLogIn)
         gotoLoginPage.setOnClickListener{
             startActivity(Intent(this, Login::class.java))
         }
     }
-}
+    fun GoToMain(view: View){
+        val username = findViewById<TextInputEditText>(R.id.user_name_register).text.toString()
+        val email = findViewById<TextInputEditText>(R.id.email_register).text.toString()
+        val password = findViewById<TextInputEditText>(R.id.password_register).text.toString()
+        val confirm_password = findViewById<TextInputEditText>(R.id.confirm_password_register).text.toString()
+
+        if (password =="" || confirm_password == "" || email == "" || username ==""){
+            Toast.makeText(applicationContext, "No field should be left Empty", Toast.LENGTH_LONG).show()
+        }else if (password == confirm_password) {
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val i = Intent(this, MainActivity::class.java)
+                        startActivity(i)
+                        Toast.makeText(applicationContext, "Registered successful", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        } else {
+            val toast = Toast.makeText(applicationContext, "Passwords don't match", Toast.LENGTH_LONG)
+            toast.show()
+        }
+    }
+    }
